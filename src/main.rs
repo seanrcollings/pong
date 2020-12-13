ce2817ce1459eb92225c1a171e6b5b1bfacf0c0f
Sean Collings <seanrcollings@gmail.com>
2020-12-12 21:30:23 -0700

    completed the tutorial

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use amethyst::{
+    audio::{AudioBundle, DjSystemDesc},
+    core::transform::TransformBundle,
+    input::{InputBundle, StringBindings},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    ui::{RenderUi, UiBundle},
+    utils::application_root_dir,
+};
+
+mod audio;
+mod pong;
+mod systems;
+use crate::audio::Music;
+use crate::pong::Pong;
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+    let app_root = application_root_dir()?;
+    let binding_path = app_root.join("config").join("bindings.ron");
+    let display_config_path = app_root.join("config").join("display.ron");
+    let input_bundle =
+        InputBundle::<StringBindings>::new().with_bindings_from_file(binding_path)?;
+
+    let game_data = GameDataBuilder::default()
+        // Bundles
+        .with_bundle(input_bundle)?
+        .with_bundle(AudioBundle::default())?
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(UiBundle::<StringBindings>::new())?
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)?
+                        .with_clear([0.0, 0.0, 0.0, 1.0]),
+                )
+                .with_plugin(RenderFlat2D::default())
+                .with_plugin(RenderUi::default()),
+        )?
+        // Systems
+        .with_system_desc(
+            DjSystemDesc::new(|music: &mut Music| music.music.next()),
+            "dj_system",
+            &[],
+        )
+        .with(systems::PaddleSystem, "paddle_system", &["input_system"])
+        .with(systems::MoveBallsSystem, "ball_system", &[])
+        .with(systems::WinnerSystem, "winner_system", &["ball_system"])
+        .with(
+            systems::BounceSystem,
+            "collision_system",
+            &["paddle_system", "ball_system"],
+        );
+
+    let assets_dir = app_root.join("assets");
+    let mut game = Application::new(assets_dir, Pong::default(), game_data)?;
+    game.run();
+    Ok(())
+}
